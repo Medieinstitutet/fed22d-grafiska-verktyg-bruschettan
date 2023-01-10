fdf4c5b1a6ce866fbda9013baf59895710684e46
existenztim <tim920129@hotmail.com>
2023-01-10 14:47:41 +0100

    :tada: Added animation for open menu content

--- menu.js
+++ menu.js
@@ -1,7 +1,22 @@
+import { gsap } from 'gsap';
+
 const menuBtn = document.querySelector('#menu-button');
 const menuNav = document.querySelector('.menu-nav');
+// const menuTl = new gsap.timeline({ repeat: 0, repeatDelay: 0 }); // for infinite repeat add -1
 
 const menuToggle = () => {
   menuNav.classList.toggle('hidden');
+  if (!menuNav.classList.contains('hidden')) {
+    gsap.to('.menu-nav', {
+      yPercent: 0,
+      duration: 1,
+      ease: 'bounce',
+    });
+  } else {
+    gsap.to('.menu-nav', {
+      yPercent: -100,
+      ease: 'none',
+    });
+  }
 };
 menuBtn.addEventListener('click', menuToggle);
