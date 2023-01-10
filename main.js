f46d1ee85f79e22113c4494ec85bd9abd7ef0927
Evelina Norlin <evelina.norlin@hotmail.com>
2023-01-10 13:57:43 +0100

    ✨added cards object

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 import './src/style/style.scss';
 // import { gsap } from 'gsap'; // kommentera ut detta om ni vill använda gsap
+
+const cards = [{
+  name: 'FireFly 300',
+  description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis.',
+  img1: 'gadget_desktop.png',
+}
+];
+
+const cardsHolder = document.querySelector('#cards-holder');
+
+cards.forEach((card) => {
+  cardsHolder.innerHTML+=`
+  <h3>${card.name}</h3>
+  <p>${card.description}</p>`
+});
