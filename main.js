bc3d19cc5170e3f9801df4e79d6d0b698f314967
Evelina Norlin <evelina.norlin@hotmail.com>
2023-01-10 15:20:35 +0100

    ✨ added swiper in card

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import './src/style/style.scss';
 // import { gsap } from 'gsap'; // kommentera ut detta om ni vill använda gsap
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js';
 
 const cards = [{
   name: 'FireFly 300',
@@ -11,7 +12,27 @@ const cards = [{
 const cardsHolder = document.querySelector('#cards-holder');
 
 cards.forEach((card) => {
-  cardsHolder.innerHTML+=`
+  cardsHolder.innerHTML += `
   <h3>${card.name}</h3>
-  <p>${card.description}</p>`
+
+  <div class="swiper mySwiper">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide"><img src="/assets/${card.img1}"></div>
+      <div class="swiper-slide"><img src="/assets/${card.img1}"></div>
+    </div>
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
+  </div>
+
+  <p>${card.description}</p>
+  
+  <button>Order</button>`;
+});
+
+// eslint-disable-next-line no-unused-vars
+const swiper = new Swiper('.mySwiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
