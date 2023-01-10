9efd680325dab4fdd77d3239c792889f1371d278
existenztim <tim920129@hotmail.com>
2023-01-10 23:09:40 +0100

    :bug: Animation will now play on first click on pageload

--- menu.js
+++ menu.js
@@ -1,9 +1,13 @@
 import { gsap } from 'gsap';
 
 const menuBtn = document.querySelector('.menu-button-container');
-const menuBtnStripes = document.querySelector('.menu-btn-stripes span:nth-child(1)');
+const menuBtnMiddleStripe = document.querySelector('.menu-btn-stripes span:nth-child(1)');
+const menuBtnTopStripe = document.querySelector('.menu-btn-stripes span:nth-child(2)');
+const menuBtnBottomStripe = document.querySelector('.menu-btn-stripes span:nth-child(3)');
 const menuNav = document.querySelector('.menu-nav');
 // const menuTl = new gsap.timeline({ repeat: 0, repeatDelay: 0 }); // for infinite repeat add -1
+
+menuNav.classList.toggle('hidden');
 
 /**
  * Menu toggle for mobile/tablet
@@ -12,30 +16,45 @@ const menuNav = document.querySelector('.menu-nav');
 const menuToggle = () => {
   menuNav.classList.toggle('hidden');
   if (!menuNav.classList.contains('hidden')) {
-    menuBtnStripes.classList.add('open');
-    gsap.to('.menu-nav', {
-      yPercent: 0,
-      duration: 1,
-      ease: 'bounce',
-    });
+    gsap.fromTo(
+      '.menu-nav',
+      { yPercent: -100 },
+      {
+        yPercent: 0,
+        ease: 'power1',
+      },
+    );
 
-    gsap.to(menuBtnStripes, {
+    gsap.to(menuBtnMiddleStripe, {
       x: -50,
       opacity: 0,
     });
+    gsap.to(menuBtnTopStripe, {
+      rotation: '45_short',
+      y: 15,
+    });
+    gsap.to(menuBtnBottomStripe, {
+      rotation: '-45_short',
+      y: 0,
+    });
   } else {
-    menuBtnStripes.classList.remove('open');
     gsap.to('.menu-nav', {
       yPercent: -100,
-      ease: 'none',
     });
 
-    gsap.to(menuBtnStripes, {
+    gsap.to(menuBtnMiddleStripe, {
       x: 0,
       opacity: 1,
+    });
+    gsap.to(menuBtnTopStripe, {
+      y: 0,
+      rotation: 0,
+    });
+    gsap.to(menuBtnBottomStripe, {
+      y: 16,
+      rotation: 0,
     });
   }
 };
 
 menuBtn.addEventListener('click', menuToggle);
-menuNav.classList.toggle('hidden');
