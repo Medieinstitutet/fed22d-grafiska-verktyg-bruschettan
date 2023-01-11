3e9661b180894be98fbebdcd4eaa452a49940853
existenztim <tim920129@hotmail.com>
2023-01-11 23:20:02 +0100

    :recycle: Simplified footer, no JS needed!

--- footer.js
+++
@@ -1,9 +0,0 @@
-const findUs = document.querySelector('.find-us-container');
-const quickLinks = document.querySelector('.quick-links-container');
-const title = document.querySelector('.title-container');
-
-findUs.classList.add('hidden-footer');
-quickLinks.classList.add('hidden-footer');
-title.classList.add('hidden-footer');
-
-// ändra class i html ist och bytt i media query
