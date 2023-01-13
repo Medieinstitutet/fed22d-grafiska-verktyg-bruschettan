7cb4fbe764e330424b0692d587a90855c4a26b29
emiliamassing <emiliamassings@gmail.com>
2023-01-13 11:07:26 +0100

    🎨💄changed header to section, positioning for desktop

--- start.js
+++ start.js
@@ -2,12 +2,12 @@ const smallerText = document.querySelector('.smaller-text');
 const largerText = document.querySelector('.larger-text');
 
 function changeTextAmount() {
-  if (window.innerWidth < 480) {
-    smallerText.classList.remove('visually-hidden');
-    largerText.classList.add('visually-hidden');
-  } else if (window.innerWidth > 480) {
+  if (window.innerWidth > 480) {
     smallerText.classList.add('visually-hidden');
     largerText.classList.remove('visually-hidden');
+  } else if (window.innerWidth < 480) {
+    smallerText.classList.remove('visually-hidden');
+    largerText.classList.add('visually-hidden');
   }
 }
 
